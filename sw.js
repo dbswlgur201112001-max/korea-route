05ce581d7a384f359b60425da3ca1e0f90b85211
dbswlgur201112001-max <dbswlgur201112001@gmail.com>
2026-08-31 00:01:46 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='korea-route-public-beta-v36-0830';
+const CACHE='korea-route-public-beta-v41-0830';
 const ASSETS=['/','/index.html','/manifest.json','/icon-192.png','/icon-512.png'];
 
 self.addEventListener('install',event=>{
