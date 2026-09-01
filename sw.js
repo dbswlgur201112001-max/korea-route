0cc84d3d933eebf00c6408822b9e676efd1259c2
dbswlgur201112001-max <dbswlgur201112001@gmail.com>
2026-09-01 11:16:11 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='korea-route-public-beta-v162-0901';
+const CACHE='korea-route-public-beta-v163-0901';
 const CORE=['/','/index.html','/foreigner-access.json','/taxi-fare.json'];
 const OPTIONAL=['/manifest.json','/icon-192.png','/icon-512.png'];
 
